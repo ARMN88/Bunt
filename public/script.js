3af021b63316481235d8315526f1b07da29d6938
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-07-09 01:48:42 -0500

    Added Player Class

--- public/script.js
+++ public/script.js
@@ -1,4 +1,17 @@
+import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
 const socket = io();
+
+const canvas = document.querySelector('canvas');
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const ctx = canvas.getContext('2d');
+
+function Update() {
+  requestAnimationFrame(Update);
+}
+
+Update();
 
 socket.on('connect', () => {
   console.log('Connected to server!');
